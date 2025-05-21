ea98c572b7d68174e0dd0e18050db9f335eb56de
Aniket.Ugale <aniket.ugale@hatchfast.dev>
2025-05-21 14:58:35 +0530

    made changes in scoring.py file

--- utils/scoring.py
+++ utils/scoring.py
@@ -1,4 +1,11 @@
 # utils/scoring.py
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 import re
 import numpy as np
